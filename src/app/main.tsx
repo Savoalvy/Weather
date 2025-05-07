10d52bd4fd7a87c91472b6655fbf74e8ea46a29f
Savoalvy <savoalvy2312@mail.ru>
2025-05-07 17:30:59 +0300

    fix_TS

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,9 +1,9 @@
 import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
 import { ChakraProvider } from '@chakra-ui/react';
 import customTheme from '../shared/theme';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
+import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
